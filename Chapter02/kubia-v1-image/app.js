3277eb0203d9c5b389e68482432972818d9fc4a0
Marko Lukša <marko.luksa@gmail.com>
2020-06-13 08:25:56 +0200

    Implement new version of kubia app

--- Chapter02/kubia-v1-image/app.js
+++ Chapter02/kubia-v1-image/app.js
@@ -1,14 +1,20 @@
 const http = require('http');
 const os = require('os');
 
+const listenPort = 8080;
+
 console.log("Kubia server starting...");
+console.log("Local hostname is " + os.hostname());
+console.log("Listening on port " + listenPort);
 
 var handler = function(request, response) {
-  console.log("Received request from " + request.connection.remoteAddress);
+  let clientIP = request.connection.remoteAddress;
+  console.log("Received request for " + request.url + " from " + clientIP);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.write("Hey there, this is " + os.hostname() + ". ");
+  response.write("Your IP is " + clientIP + ". ");
+  response.end("\n");
 };
 
 var server = http.createServer(handler);
-server.listen(8080);
-
+server.listen(listenPort);
