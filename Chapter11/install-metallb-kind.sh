55f707bd786abdcd282610c6cd9b6714cb1079e9
Marko Lukša <marko.luksa@gmail.com>
2021-12-19 11:52:30 +0100

    Add bash shebang to install-metallb-kind.sh

--- Chapter11/install-metallb-kind.sh
+++ Chapter11/install-metallb-kind.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml
