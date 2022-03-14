6f90fbfad11d392a6144e2c0c12a37b936b25e33
Marko Lukša <marko.luksa@gmail.com>
2022-03-14 22:11:29 +0100

    Remove quiz-db-initializer-0.1 container image

--- container-images/quiz-db-initializer-0.1/import-questions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Importing quiz questions from /import/qestions.json into MongoDB"
-mongoimport --db kiada --collection questions --file /import/questions.json --jsonArray
-echo "Import finished."
